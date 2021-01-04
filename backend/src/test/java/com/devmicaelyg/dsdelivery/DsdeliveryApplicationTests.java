46479910448678b721cdc211f787fcd1f9588f76
devmicaelyg <gusmaomicaely@gmail.com>
2021-01-04 17:38:38 -0300

    Project Created

---
+++ backend/src/test/java/com/devmicaelyg/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmicaelyg.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
