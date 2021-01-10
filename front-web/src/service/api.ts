af404645327ed4a269d5730bdae2ee96b03de86d
devmicaelyg <gusmaomicaely@gmail.com>
2021-01-10 16:45:25 -0300

    change url

--- front-web/src/service/api.ts
+++ front-web/src/service/api.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export const fetchProducts = () => {
-    return axios(`${API_URL}/products`);
+    return axios(`${API_URL}products`);
 }
 
 export const fetchLocalMapBox = (local:string) => {
