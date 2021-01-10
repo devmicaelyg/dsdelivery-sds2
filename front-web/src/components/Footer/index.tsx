2bb1df38e77d20bf98b84da967bf58d29537a690
devmicaelyg <gusmaomicaely@gmail.com>
2021-01-10 16:03:35 -0300

    Changes in the footer links

--- front-web/src/components/Footer/index.tsx
+++ front-web/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
-import './styles.css';
 import { ReactComponent as InstagramLogo} from '../../assets/Instagram.svg';
 import { ReactComponent as LinkedinLogo} from '../../assets/Linkedin.svg';
 import { ReactComponent as YoutubeLogo } from '../../assets/youtube.svg';
+import './styles.css';
 
 
 const Footer = () => {
@@ -9,13 +9,13 @@ const Footer = () => {
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="#" target="_new">
+                <a href="https://www.instagram.com/devmicaelyg" target="_new">
                     <InstagramLogo />
                 </a>
-                <a href="#" target="_new">
+                <a href="https://www.linkedin.com/in/micaely-gusm%C3%A3o-23b8641b0/" target="_new">
                     <LinkedinLogo />
                 </a>
-                <a href="#" target="_new">
+                <a href="https://www.youtube.com" target="_new">
                     <YoutubeLogo />
                 </a>
             </div>
